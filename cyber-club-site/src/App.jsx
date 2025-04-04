08c1c632bf6b362be62f5211ea6c05bae56c1211
EnzoSeekMaro <enzo.moro@seekmaro.com>
2025-04-03 20:37:09 -0700

    Day 2# coded light mode and dark mode to change collor theme of toolbar + whenever change the theme letters and logo changes collors

--- cyber-club-site/src/App.jsx
+++ cyber-club-site/src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes, BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, useTheme } from './components/ThemeController';
 import About from './components/About';
 import FAQ from './components/FAQ';
 import EventInfo from './components/EventInfo';
 import Board from './components/Board';
 import Linktree from './components/Linktree';
-import cyberlogo from '/cyberlogo.png';
+import cyberlogoblack from '/Cyber_Logo_Black.png';
+import cyberlogowhite from '/Cyber_Logo_White.png';
 import './App.css';
 
 function App() {
@@ -18,31 +19,89 @@ function App() {
     setIsDarkMode(!isDarkMode); // Toggle the icon state
   };
 
+  const linkHoverStyle = {
+    textShadow: isDarkMode
+      ? '0 0 10px #ffffff, 0 0 20px #ffffff' // White glow for dark mode
+      : '0 0 10px #000000, 0 0 20px #000000', // Black glow for light mode
+  };
+
   return (
     <div style={{ backgroundColor: theme.secondaryColor, color: theme.primaryColor }}>
       <Router>
         <div>
-          <div className="toolbar-container">
-          <img src={cyberlogo} className="logo" alt="CyberLogo" />
+          <div
+            className="toolbar-container"
+            style={{
+              background: theme.toolbarBackgroundGradient,
+              color: theme.toolbarFontColor,
+            }}
+          >
+            <img
+              src={isDarkMode ? cyberlogowhite : cyberlogoblack}
+              className="logo"
+              alt="CyberLogo"
+            />
             <nav className="toolbar">
               <ul>
                 <li>
-                  <Link to="/">Home</Link>
+                  <Link
+                    style={{ color: theme.toolbarFontColor }}
+                    onMouseEnter={(e) => (e.target.style.textShadow = linkHoverStyle.textShadow)}
+                    onMouseLeave={(e) => (e.target.style.textShadow = 'none')}
+                    to="/"
+                  >
+                    Home
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/about">About</Link>
+                  <Link
+                    style={{ color: theme.toolbarFontColor }}
+                    onMouseEnter={(e) => (e.target.style.textShadow = linkHoverStyle.textShadow)}
+                    onMouseLeave={(e) => (e.target.style.textShadow = 'none')}
+                    to="/about"
+                  >
+                    About
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/faq">FAQ</Link>
+                  <Link
+                    style={{ color: theme.toolbarFontColor }}
+                    onMouseEnter={(e) => (e.target.style.textShadow = linkHoverStyle.textShadow)}
+                    onMouseLeave={(e) => (e.target.style.textShadow = 'none')}
+                    to="/faq"
+                  >
+                    FAQ
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/events">Events</Link>
+                  <Link
+                    style={{ color: theme.toolbarFontColor }}
+                    onMouseEnter={(e) => (e.target.style.textShadow = linkHoverStyle.textShadow)}
+                    onMouseLeave={(e) => (e.target.style.textShadow = 'none')}
+                    to="/events"
+                  >
+                    Events
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/board">Board</Link>
+                  <Link
+                    style={{ color: theme.toolbarFontColor }}
+                    onMouseEnter={(e) => (e.target.style.textShadow = linkHoverStyle.textShadow)}
+                    onMouseLeave={(e) => (e.target.style.textShadow = 'none')}
+                    to="/board"
+                  >
+                    Board
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/linktree">Linktree</Link>
+                  <Link
+                    style={{ color: theme.toolbarFontColor }}
+                    onMouseEnter={(e) => (e.target.style.textShadow = linkHoverStyle.textShadow)}
+                    onMouseLeave={(e) => (e.target.style.textShadow = 'none')}
+                    to="https://linktr.ee/csulbcybersecurity?utm_source=linktree_admin_share"
+                  >
+                    Linktree
+                  </Link>
                 </li>
               </ul>
             </nav>
@@ -50,15 +109,14 @@ function App() {
               {isDarkMode ? '🌙' : '☀️'}
             </button>
           </div>
-
-          <Routes>
-            <Route path="/about" element={<About />} />
-            <Route path="/faq" element={<FAQ />} />
-            <Route path="/events" element={<EventInfo />} />
-            <Route path="/board" element={<Board />} />
-            <Route path="/linktree" element={<Linktree />} />
-            <Route path="/" element={<Home />} />
-          </Routes>
+            <Routes>
+              <Route path="/about" element={<About />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/events" element={<EventInfo />} />
+              <Route path="/board" element={<Board />} />
+              <Route path="/linktree" element={<Linktree />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
         </div>
       </Router>
     </div>
--- cyber-club-site/src/components/ThemeController.jsx
+++ cyber-club-site/src/components/ThemeController.jsx
@@ -2,17 +2,25 @@ import React, { createContext, useState, useContext } from 'react';
 
 const ThemeContext = createContext();
 
+const lightTheme = {
+  primaryColor: '#000000',
+  secondaryColor: '#ffffff',
+  toolbarBackgroundGradient: 'linear-gradient(150deg, rgba(235,237,255,0.6363139005602241) 23%, rgba(201,201,201,0.5494791666666667) 47%, rgba(240,243,255,0.7175463935574229) 70%)',
+  toolbarFontColor: '#000000',
+};
+
+const darkTheme = {
+  primaryColor: '#ffffff',
+  secondaryColor: '#000000',
+  toolbarFontColor: '#ffffff',
+  toolbarBackgroundGradient: 'linear-gradient(150deg, rgba(24,20,83,0.9388349089635855) 25%, rgba(38,19,62,0.9416360294117647) 47%, rgba(54,24,88,0.9388349089635855) 70%)',
+};
+
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState({
-    primaryColor: '#1D2E6C',
-    secondaryColor: '#FFFFFF',
-  });
+  const [theme, setTheme] = useState(lightTheme);
 
   const toggleTheme = () => {
-    setTheme(prevTheme => ({
-      primaryColor: prevTheme.primaryColor === '#1D2E6C' ? '#FFFFFF' : '#1D2E6C',
-      secondaryColor: prevTheme.secondaryColor === '#FFFFFF' ? '#1D2E6C' : '#FFFFFF',
-    }));
+    setTheme(prevTheme => (prevTheme === lightTheme ? darkTheme : lightTheme));
   };
 
   return (
